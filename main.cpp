bc54d0a33c8221ef6194b88f01eaa74c6b9c47df
Pablo Sanchez <14944505+Manoloon@users.noreply.github.com>
2022-01-31 09:19:02 -0300

    ranges

--- main.cpp
+++ main.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
 #include <ranges>
+#include <vector>
 #include <concepts>
 
 int main()
 {
+    // Ranges -> nothing works because this impl should use rangesV3 .
+    // convert each element into a pair of index and value
+    std::vector<double> scene_powers = {100,105,53,59.3,69,54,65};
+    auto face_powers = scene_powers | std::ranges::views::enumerate
+                                    // keep only elements where numbers are increase by >5 from previous
+                                    | std::ranges::views::adjacent_filter(
+                                            [](const auto &p1,const auto &p2)
+                                            {
+                                                return p2.second - p1.second > 5;
+                                            })
+                                    // drop the first value , its not significant.
+                                    | std::ranges::views::tail;
+
+    std::vector<int> ints{3,54,1,2,6132,21};
+    // pipeline
+    auto result = ints  | std::ranges::views::filter([](int i) {return i%2 == 0;}
+                        | std::ranges::views::transform([](int i) {return std::to_string(i);});
+    std::cout << result << std::endl;
+
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
